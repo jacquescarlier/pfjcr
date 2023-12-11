05e0703920021489d079d885d0cd4c9da6131e3a
jacques <lapitou60@gmail.com>
2023-12-11 09:56:59 +0100

    style modification

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,8 @@ function Footer() {
     <footer>
       <p className="contact">contact</p>
       <ul>
-      <li><a href="#"> <img src="./images/email.png" alt="logo email" /> </a></li>
+     
+      <li><a href="mailto:jacques.carlier@gmail.com"> <img src="./images/email.png" alt="logo email" id="email" /></a></li>
         <li> <a href="https://github.com/jacquescarlier/" target="_blank" rel="noopener"> <img src="./images/github.webp" alt="logo github" /> </a> </li>
         <li><a href="https://www.linkedin.com/in/jacques-carlier-9a39121bb?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener"> <img src="./images/linkedin.webp" alt="logo linkedin" /> </a></li>
         
--- src/components/project/ProjectContainer.jsx
+++ src/components/project/ProjectContainer.jsx
@@ -1,15 +1,41 @@
 export default function ProjectContainer() {
+  "use strict";
+  window.addEventListener("DOMContentLoaded", (event) => {
+    animate_text();
+  });
+  function animate_text() 
+{
+  let delay = 100,
+      delay_start = 0,
+      contents,
+      letters;
 
+  document.querySelectorAll(".animate-text").forEach(function (elem) {
+    contents = elem.textContent.trim();
+    elem.textContent = "";
+    letters = contents.split("");
+    elem.style.visibility = 'visible';
+
+    letters.forEach(function (letter, index_1) {
+      setTimeout(function () {
+        // effet machine à écrire 
+        elem.textContent += letter;
+      }, delay_start + delay * index_1);
+    });    
+    delay_start += delay * letters.length;
+  });
+}
+animate_text()
   return (
     <div className="projectTitle">
       <h2 className="bloc">
-        <span>P</span>
-        <span>r</span>
-        <span>o</span>
-        <span>j</span>
-        <span>e</span>
-        <span>t</span>
-        <span>s</span>
+        <span className="letter01">P</span>
+        <span className="letter02">r</span>
+        <span className="letter03">o</span>
+        <span className="letter04">j</span>
+        <span className="letter05">e</span>
+        <span className="letter06">t</span>
+        <span className="letter07">s</span>
       </h2>
       <div className="imgProject">
         <img src="./langages/HTML51.webp" alt="logo html" title="HTML" />
