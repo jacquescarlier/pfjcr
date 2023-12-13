633fdb97e64bf5c3265dcc34c3b340bfcb396786
jacques <lapitou60@gmail.com>
2023-12-13 14:54:47 +0100

    modification of the header and the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,6 @@ function Footer() {
         <li><a href="mailto:jacques.carlier@gmail.com"> <img src="./images/emailpf.webp" alt="logo email" id="email" /></a></li>
         <li> <a href="https://github.com/jacquescarlier/" target="_blank" rel="noopener"> <img src="./images/github.webp" alt="logo github" /> </a> </li>
         <li><a href="https://www.linkedin.com/in/jacques-carlier-9a39121bb?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener"> <img src="./images/linkedin.webp" alt="logo linkedin" /> </a></li>
-
       </ul>
       <span className="topLink"><a href="#top">TOP</a></span>
     </footer>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,11 @@
-import logoJcr from "../../pictures/logo/logoprint.webp";
 import Navigation from "../navigation/Navigation";
 
 function Header() {
   return (
     <div className="header" id="top">
-      <div className="logoRotate"> <span>JCR</span></div>
-      { /* <img src={logoJcr} alt="logo jcr" /> */ }
+      <div className="logoRotate">
+        <span>JCR</span>
+      </div>
       <Navigation />
     </div>
   );
