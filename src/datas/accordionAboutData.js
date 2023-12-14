d81c5a2fb54927e0b76e24088b905fb73c362b8a
jacques <lapitou60@gmail.com>
2023-12-14 09:35:41 +0100

    clearing unused variables

--- src/datas/accordionAboutData.js
+++ src/datas/accordionAboutData.js
@@ -2,8 +2,9 @@ export const accordionData = [
   {
     id: 1,
     title: "Expérience",
-    content: `Après plus de 30 années dans l'industrie en tant que technicien de production, de recherche et développement, qui m'ont permis d’acquérir des compétences telles que l’organisation, l’écoute et l'apprentissage en continu de nouvelle technologie.
-    J’ai profité d'un concours de circonstance pour effectuer une reconversion professionnelle dans le développement Web, domaine qui m’intéressait depuis de nombreuses années. `,
+    content: `Après plus de 30 années dans l'industrie en tant que technicien de production, de recherche et développement, celle ci m'ont permis d’acquérir des compétences telles que l’organisation, l’écoute et l'apprentissage en continu de nouvelle technologie.
+    J’ai profité d'un concours de circonstance pour effectuer une reconversion professionnelle dans le développement Web, domaine qui m’intéressait depuis de nombreuses années.
+    J'ai suivi une formation d'intégrateur web de niveau 5 `,
   },
   {
     id: 2,
