a97066a5d6f27009b7090fd6651b5869fcbbb1d9
jacquescarlier <jacques.carlier@gmail.com>
2024-01-23 11:26:13 +0100

    backup

---
+++ src/datas/data.js
@@ -0,0 +1,47 @@
+  export const AboutData = [
+  {
+    id: 1,
+    title: "Expérience",
+    content: `Après plus de 30 années dans l'industrie en tant que technicien de production, de recherche et développement, qui m'ont permis d’acquérir des compétences telles que l’organisation, l’écoute et l'apprentissage en continu de nouvelle technologie.
+    J’ai profité d'un concours de circonstance pour effectuer une reconversion professionnelle dans le développement Web, domaine qui m’intéressait depuis de nombreuses années.5 `,
+  },
+
+];
+
+export const imgLangages = [
+  {
+    id: 101,
+    alt: "logo Html",
+    img: "./logo-htmlpf.webp"
+  },
+  {
+    id: 102,
+    alt: "logo Css",
+    img: "./csspf.webp"
+  },
+  {
+    id: 103,
+    alt: "logo Sass",
+    img: "./Sass1.webp"
+  },
+  {
+    id: 104,
+    alt: "logo Java script",
+    img: "./Javascript1.webp"
+  },
+  {
+    id: 105,
+    alt: "logo React",
+    img: "./react-logopf.webp"
+  },
+  {
+    id: 106,
+    alt: "logo Redux",
+    img: "./redux1.webp"
+  },
+  {
+    id: 107,
+    alt: "logo Seo",
+    img: "./seo.webp"
+  }
+]
