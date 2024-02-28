e39a1869e02f638fc0cd0d31162e2e8925096554
jacquescarlier <jacques.carlier@gmail.com>
2024-02-28 09:48:47 +0100

    add backlink

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,6 +8,12 @@ function Footer() {
         <li><a href="https://www.linkedin.com/in/jacques-carlier-9a39121bb?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener"> <img src="./images/linkedin.webp" alt="logo linkedin" /> </a></li>
       </ul>
       <span className="topLink"><a href="#top">TOP</a></span>
+      <ul className="backlinks">
+        <li><a href="https://validator.w3.org/#validate_by_uri" target="_blank" rel="noopener">Validateur W3C</a> </li>
+        <li><a href="https://developer.mozilla.org/fr/" target="_blank" rel="noopener">MDN Doc</a> </li>
+        <li><a href="https://fr.react.dev/" target="_blank" rel="noopener">React</a></li>
+        <li><a href="https://www.google.com/" target="_blank" rel="noopener">Google</a></li>
+      </ul>
     </footer>
   );
 }
