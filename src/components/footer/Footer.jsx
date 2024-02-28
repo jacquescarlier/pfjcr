edc83a3b7cda5178658aac5762b748e8eefa77f4
jacquescarlier <jacques.carlier@gmail.com>
2024-02-28 15:08:45 +0100

    delete console error

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,10 +9,10 @@ function Footer() {
       </ul>
       <span className="topLink"><a href="#top">TOP</a></span>
       <ul className="backlinks">
-        <li><a href="https://validator.w3.org/#validate_by_uri" target="_blank" rel="noopener">Validateur W3C</a> </li>
-        <li><a href="https://developer.mozilla.org/fr/" target="_blank" rel="noopener">MDN Doc</a> </li>
-        <li><a href="https://fr.react.dev/" target="_blank" rel="noopener">React</a></li>
-        <li><a href="https://www.google.com/" target="_blank" rel="noopener">Google</a></li>
+        <li><a className="tooltip" href="https://validator.w3.org/#validate_by_uri" target="_blank" rel="noopener">Validateur W3C <em>Contrôler votre site</em></a> </li>
+        <li><a className="tooltip" href="https://developer.mozilla.org/fr/" target="_blank" rel="noopener">MDN Doc<em>Documentation Mozilla</em></a> </li>
+        <li><a className="tooltip" href="https://fr.react.dev/" target="_blank" rel="noopener">React<em>Documentation React</em></a></li>
+        <li><a className="tooltip" href="https://www.google.com/" target="_blank" rel="noopener">Google<em>Moteur de recherche</em></a></li>
       </ul>
     </footer>
   );
