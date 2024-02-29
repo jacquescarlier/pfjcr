f82562be6ceebe4fd2450c096fa14a0c907c7aee
jacquescarlier <jacques.carlier@gmail.com>
2024-02-29 10:54:45 +0100

    delete page 404 and project

--- src/components/email/Email.jsx
+++ src/components/email/Email.jsx
@@ -2,13 +2,13 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser'
 
 export default function Email() {
-   
+
     const form = useRef();
     const sendEmail = (e) => {
         e.preventDefault();
         emailjs.sendForm(
             'service_11i6ecf', 'template_11i6ecf', form.current, 'e7n4vc_R24IMqBV8Y'
-           )
+        )
             .then((result) => {
                 alert('message sent successfully...');
                 form.current.reset();
@@ -19,12 +19,12 @@ export default function Email() {
     };
 
     return (
-        <div className= "contactForm">
+        <div className="contactForm">
             <h2>Contact Form</h2>
             <form className='cf' ref={form} onSubmit={sendEmail}>
                 <div className='half left cf'>
-                    <input id="name" type='text' placeholder='Name' name='user_name' aria-labelledby="name" autoComplete='off'  required />
-                    <input type='email' placeholder='Email address' name='user_email'    aria-labelledby="email" required/>
+                    <input id="name" type='text' placeholder='Name' name='user_name' aria-labelledby="name" autoComplete='off' required />
+                    <input type='email' placeholder='Email address' name='user_email' aria-labelledby="email" required />
                 </div>
                 <div className='half right cf'>
                     <textarea id="message" name='message' type='text' placeholder='Message' aria-labelledby="message" required ></textarea>
--- src/components/routing/Router.jsx
+++ src/components/routing/Router.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
-import Project from "../../pages/Project"
 import Page404 from "../../pages/Page404";
 
 function Router() {
@@ -11,8 +10,6 @@ function Router() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project/:id" element={<Project />} />
-          <Route path="/project/*" element={<Page404 />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
