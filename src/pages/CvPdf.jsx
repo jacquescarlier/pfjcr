925a84ba650f3e04e4230ecfc90ab83f3843658b
jacquescarlier <jacques.carlier@gmail.com>
2024-03-07 17:32:24 +0100

    work on pdf preview

--- src/pages/CvPdf.jsx
+++ src/pages/CvPdf.jsx
@@ -6,14 +6,15 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const Curiculum = () => {
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+        sidebarTabs: (defaultTabs) => [],
+    });
     window.scrollTo(0, 0)
     return (
         <div className='container-general'>
             <div className='retour'>
-                <Link
-                    to="/"
-                >
+                <Link to="/">
                     <li>Home</li>
                 </Link>
                 <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
