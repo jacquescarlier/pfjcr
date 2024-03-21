ce6a54c0452f63a7f1c31f4e42f7008ba2a884c4
jacquescarlier <jacques.carlier@gmail.com>
2024-03-21 14:11:33 +0100

    add a scaling effect to header links

--- src/components/projectNavigation/ProjectNavigation.jsx
+++ src/components/projectNavigation/ProjectNavigation.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const ProjectNavigation = () => {
-  return (
+ /* return (
     <div className="project-navigation">
       <ul className="navigation">
         <li><NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>Accueil</NavLink></li>
       </ul>
     </div>
-  );
+  );*/
 };
 
 export default ProjectNavigation;
