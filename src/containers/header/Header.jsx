9a9f8366cde2cf978e7c1c2a5b11ffbfe334c014
jacquescarlier <jacques.carlier@gmail.com>
2024-03-23 19:37:13 +0100

    code reorganization

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,13 @@
-import Navigation from "../../components/navigation/Navigation";
-
 function Header() {
   return (
     <div className="header" id="top">
       <span className="logoRotate">JCR</span>
-      <Navigation />
+      
+      <ul className="navigation">
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      
     </div>
   );
 }
