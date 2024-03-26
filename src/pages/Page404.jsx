b6ff58d41d03d73d149fd1cc9e3e879a7263babd
jacquescarlier <jacques.carlier@gmail.com>
2024-03-26 12:49:18 +0100

    modification of 404 page & obsolete codes

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import Footer from "../containers/footer/Footer";
-import ProjectNavigation from "../components/projectNavigation/ProjectNavigation";
+import { Link } from "react-router-dom";
 
 const Page404 = () => {
   return (
     <div>
       <div className="container-general">
-        <ProjectNavigation />
         <div className="page404">
           <h1>404</h1>
           <p>Oups! La page que vous demandez n'existe pas.</p>
+          <Link
+            to={"./"}>
+            <p>Go home</p>
+          </Link>
         </div>
       </div>
-
     </div>
   );
 };
