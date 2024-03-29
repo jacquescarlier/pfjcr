3147bed6886c2b251a5b35c45f949bde48503d0c
jacquescarlier <jacques.carlier@gmail.com>
2024-03-29 16:49:18 +0100

    changing About

--- src/datas/data.js
+++ src/datas/data.js
@@ -3,7 +3,7 @@
     id: 1,
     title: "Expérience",
     content: `Après plus de 30 années dans l'industrie en tant que technicien de production, de recherche et développement, qui m'ont permis d’acquérir des compétences telles que l’organisation, l’écoute et l'apprentissage en continu de nouvelle technologie.
-    J’ai profité d'un concours de circonstance pour effectuer une reconversion professionnelle dans le développement Web, domaine qui m’intéressait depuis de nombreuses années. Je peux enfin m'orienter vers un métier qui touche à mes intérêts personnels `,
+    J’ai profité d'un concours de circonstance pour effectuer une reconversion professionnelle dans le développement Web, domaine qui m’intéressait depuis de nombreuses années. Je suis motivé pour apprendre de nouveaux framework, langages et m'ouvrir l'esprit à de nouvelles technologies.`,
   },
 
 ];
